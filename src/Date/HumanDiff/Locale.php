522409582eb55fea4ecb7e199073a64ab54ad309
Christian Weiske <cweiske@cweiske.de>
2012-06-04 16:55:16 +0200

    class docs for locale classes

--- src/Date/HumanDiff/Locale.php
+++ src/Date/HumanDiff/Locale.php
@@ -1,11 +1,32 @@
 <?php
+/**
+ * Part of Date_HumanDiff
+ *
+ * PHP version 5
+ *
+ * @category Date
+ * @package  Date_HumanDiff
+ * @author   Christian Weiske <cweiske@php.net>
+ * @license  http://www.gnu.org/copyleft/lesser.html LGPL
+ * @link     http://pear.php.net/package/Date_HumanDiff
+ */
 
+/**
+ * Interface that all locale/translation classes need to implement.
+ *
+ * @category Date
+ * @package  Date_HumanDiff
+ * @author   Christian Weiske <cweiske@php.net>
+ * @license  http://www.gnu.org/copyleft/lesser.html LGPL
+ * @version  Release: @package_version@
+ * @link     http://pear.php.net/package/Date_HumanDiff
+ */
 interface Date_HumanDiff_Locale
 {
     /**
      * Get the translation for the given string.
      *
-     * @param string $string String to translate
+     * @param string  $string String to translate
      * @param integer $number Number to render into the string
      *
      * @return string Translated string
--- src/Date/HumanDiff/LocaleArray.php
+++ src/Date/HumanDiff/LocaleArray.php
@@ -1,6 +1,31 @@
 <?php
+/**
+ * Part of Date_HumanDiff
+ *
+ * PHP version 5
+ *
+ * @category Date
+ * @package  Date_HumanDiff
+ * @author   Christian Weiske <cweiske@php.net>
+ * @license  http://www.gnu.org/copyleft/lesser.html LGPL
+ * @link     http://pear.php.net/package/Date_HumanDiff
+ */
 require_once 'Date/HumanDiff/Locale.php';
 
+/**
+ * Abstract implementation of the locale/translation interface that
+ * works on a translation array to provide translations.
+ *
+ * It makes adding new translations very easy.
+ * Just define a $trans array.
+ *
+ * @category Date
+ * @package  Date_HumanDiff
+ * @author   Christian Weiske <cweiske@php.net>
+ * @license  http://www.gnu.org/copyleft/lesser.html LGPL
+ * @version  Release: @package_version@
+ * @link     http://pear.php.net/package/Date_HumanDiff
+ */
 abstract class Date_HumanDiff_LocaleArray implements Date_HumanDiff_Locale
 {
     /**
